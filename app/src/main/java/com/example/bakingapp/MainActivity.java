cba9493d594ae3c01ecb2dbd64944576b6e07668
Musa Tondolo <mtondolo@gmail.com>
2020-05-18 15:32:26 +0200

    Add basic layout to display list of recipes

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.bakingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mRecipeListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Using findViewById, we get a reference to our TextView from xml.
+        mRecipeListTextView = (TextView) findViewById(R.id.tv_recipes);
+
+        // This String array contains recipe.
+        String[] recipes = Recipe.getRecipe();
+
+        // Iterate through the array and append the Strings to the TextView
+        for (String recipe : recipes) {
+            mRecipeListTextView.append(recipe + "\n\n\n");
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/bakingapp/Recipe.java
@@ -0,0 +1,13 @@
+package com.example.bakingapp;
+
+public class Recipe {
+    // This method returns a list of recipes.
+    public static String[] getRecipe() {
+        return new String[]{
+                "Nutella Pie",
+                "Brownies",
+                "Yellow Cake",
+                "Cheesecake",
+        };
+    }
+}
